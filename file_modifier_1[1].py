7cf80560c49d7db109c402b595d328e880788084
swati12jaiswal <jaiswalswati94@gmail.com>
2014-07-17 12:49:33 +0530

    can descend to sub directories in current directory and perform the task

--- file_modifier_1[1].py
+++ file_modifier_1[1].py
@@ -1,29 +1,44 @@
 import os
 
-for each_file in os.listdir(r'./'):
-    #opening the file for reading
-    old_file = open(each_file,"r")
-    
-    is_first_occurence = True
+def in_dir(dir_):
+    #for each subdir in input directory
+    for each_file in os.listdir(dir_+"/"):
 
-    lines_in_file = old_file.readlines()                        #lines in the file as a list of strings; where each string is a line
-    old_file.close()
+        #if the current item is not a dir
+        if not os.path.isdir(each_file):
+            if each_file[5:9] == '.txt':
+                #opening the file for reading
+                old_file = open(dir_+"/"+each_file,"r")                                     
 
-    #opening the file for writing the modified version
-    old_file = open(each_file,"w")
+                if not old_file:
+                    print "couldn't open"
+                    
+                is_first_occurence = True
+                
+                #lines in the file as a list of strings; where each string is a line
+                lines_in_file = old_file.readlines()                                         
+                old_file.close()
+                
+                #opening the file for writing the modified version
+                old_file = open(dir_+"/"+each_file,"w")                                      
 
-    #iterating over the lines_in the file as strings.
-    for line in lines_in_file:
-        new_line=line
-        if is_first_occurence:                                  #for the first occurence of the word 
-            location_of_var = line.find("require once")
-            is_first_occurence = False
+                #iterating over the lines_in the file as strings.
+                for line in lines_in_file:                                                     
+                    new_line=line
+                    #if the first occurence of the word
+                    if is_first_occurence:                                                    
+                        location_of_var = line.find("require once")
+                        is_first_occurence = False
+                    else:
+                        location_of_var = line.find("require once",location_of_var+1)    
+                    if location_of_var != -1:        #text found                                        
+                        length_of_word = len("require once")
+                        add_the_text_at = location_of_var + length_of_word
+                        new_line = line[:add_the_text_at+1]+"text"+line[add_the_text_at:]
+                    #writing the modified line to the file    
+                    old_file.write(new_line)                                                      
+                old_file.close()
         else:
-            location_of_var = line.find("require once",location_of_var+1)
-        if location_of_var != -1:                               #text found
-            length_of_word = len("require once")
-            add_the_text_at = location_of_var + length_of_word
-            new_line = line[:add_the_text_at+1]+"text"+line[add_the_text_at:]
-        old_file.write(new_line)                                #writing the modified line to the file     
-      
-    old_file.close()
+            #recursively descending the directory structure
+            in_dir(dir_+"/"+each_file)
+in_dir(r'.')
