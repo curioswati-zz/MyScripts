e74fff991ead37cc1bf2a046081e398fb8108033
swati12jaiswal <jaiswalswati94@gmail.com>
2014-07-17 17:55:42 +0530

    modular and well optimized as now it reads the file only once

--- csv_reader.py
+++ csv_reader.py
@@ -1,24 +1,27 @@
 import csv
-reader = csv.reader(open("Choose Your Big Buddy - Sheet1.csv"))
-bList = {}
-c=True
-big_buddies = {'Ankit Kulkarni':0,'Ajeet Khan':0,'Akanksha Rathore':0,'Rahul Sagore':0,'Shaifali Agrawal':0,'Chitrank Dixit':0,'Nitesh Mishra':0,'Komal Rathore':0,'Bedi Singh Yadav':0,'Sunny Raikwar':0,'Gaurav Parmar':0}
-for row in reader:
-    if c:
-        for i in range(0,len(row)):
-            bList[row[i]] = []
-        c=False
-    else:
-        i=0
-        for key in bList:
-            bList[key].append(row[i])
-            i+=1
-cnt=20
-for key in bList:
-    for name in big_buddies:
-        if key != 'NewBies':
-            score = bList[key].count(name) * cnt
-            #print cnt
-            big_buddies[name]+=score
-    cnt-=5
-print bList#big_buddies
+
+def score(name,dic,index):
+    score = [15,10,5]
+    dic[name]+=score[index-1]
+    
+def add_entry(name,dic,index):
+    if name in dic:
+        score(name,dic,index)
+        return True
+    elif name != "":
+        dic[name] = 0
+        score(name,dic,index)
+        return False
+def main():
+    reader = csv.reader(open("C:\Python27\Choose Your Big Buddy - Sheet1.csv"))
+    c=True
+    big_buddies = {}
+    for row in reader:
+        if c:
+           c=False
+        else:
+            for i, col in enumerate(row):
+                if i != 0:
+                    add_entry(col,big_buddies,i)
+    return big_buddies                
+print main()
