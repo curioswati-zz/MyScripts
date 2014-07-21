8c07e11f1bc5db3da97f8a7077ed5363c1e95296
swati12jaiswal <jaiswalswati94@gmail.com>
2014-07-21 17:00:46 +0530

    Takes the texts as input from user

--- file_modifier_1[1].py
+++ file_modifier_1[1].py
@@ -1,8 +1,9 @@
-'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 ''This script will modify all the files in the current directory.
 It works recursively on all subdirectories.
-It will insert the input string on a required place as mentioned with respect to some text.''
-'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+It will insert the input string on a required place as mentioned with respect to some text.
+There are two user inputs, first is the text after which to append and second is the text to append''
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 ''''''''''''''''''''''''''''''''''''''''''''
 '''Required Modules'''
 ''''''''''''''''''''''''''''''''''''''''''''
@@ -14,10 +15,10 @@ Each line is a string.''
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 def read_file(input_file):
     try:
-        inputFile = open(input_file,'r')                                  #opening the input_file for reading.
+        inputFile = open(input_file,'r')                                    #opening the input_file for reading.
         return inputFile.readlines()
     except:
-        print "There was some problem with file "+input_file+"."          #if any file not exist or fails to open, The program will not stop.
+        print "There was some problem with file "+input_file+"."            #if any file not exist or fails to open, The program will not stop.
         
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 ''Writes the input lines in the out_file.
@@ -31,34 +32,36 @@ def write_file(out_file,lines):
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 ''Modifies a line by placing some text on a location after some text.
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-def modify_line(line,location):                                                            
+def modify_line(line,insert_after,text):                                                            
     new_line=line
-    location = line.find("require once",location)
-    if location != -1:                                                    #text found
-        length_of_word = len("require once")
+    location = line.find(insert_after)
+    if location != -1:                                                       #text found        
+        length_of_word = len(insert_after)
         add_the_text_at = location + length_of_word
-        new_line = line[:add_the_text_at+1]+"text"+line[add_the_text_at:] #new_line, assigned the old line with insertion of the text.
-    return new_line,location+1                                            #returning the new_line with the location after, text was found.  
+        new_line = line[:add_the_text_at+2]+text+line[add_the_text_at+2:]    #new_line, assigned the old line with insertion of the text.
+    return new_line                                                          #returning the new_line with the location after, text was found.  
 
 
 '''''''''''''''''
 The main module
 '''''''''''''''''
-def main(dir_):              
-    for each_file in os.listdir(dir_+"/"):                                #for each file in input dir_.       
-        if not os.path.isdir(each_file):                                  #if the file is not a directory.  
-            file_extension = each_file[5:9]
-
-            if file_extension == '.txt':
-                lines_in_file = read_file(dir_+"/"+each_file)             #reading file                                
-                lines_to_write = []                                       #list to write new lines        
-                location_of_text = 0 
+def main(dir_,insert_after,insert_text):
+    for each_file in os.listdir(dir_+"/"):                                    #for each file in input dir_.
+        if not os.path.isdir(dir_+"/"+each_file):                             #if the file is not a directory.  
+            
+            if not each_file.endswith('.py'):
+                print each_file    
+                lines_in_file = read_file(dir_+"/"+each_file)                 #reading file                                
+                lines_to_write = []                                           #list to write new lines        
                                                
-                for line in lines_in_file:                                #iterating over the lines_in_file.
-                    line, location_of_text = modify_line(line,location_of_text)
-                    lines_to_write.append(line)                           #appending the line to list
+                for line in lines_in_file:                                    #iterating over the lines_in_file.
+                    line = modify_line(line,insert_after,insert_text)
+                    lines_to_write.append(line)                               #appending the line to list
                     
-                write_file(dir_+"/"+each_file, lines_to_write)            #writing the new lines to file.
+                write_file(dir_+"/"+each_file, lines_to_write)                #writing the new lines to file.
         else:
-            main(dir_+"/"+each_file)                                      #Go through the subdirectory.
-main(r'.')                                                                #calling the main() with current directory
+            main(dir_+"/"+each_file,insert_after,insert_text)                 #Go through the subdirectory.
+
+insert_after = raw_input("Enter the text after which you want to append : ")
+insert_text = raw_input("Enter the text to append: ")
+main(r'.',insert_after,insert_text)                                           #calling the main() with current directory
