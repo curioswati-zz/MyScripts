4b6890f5a57ec6111b7d37c1d9f02c136cb69124
swati12jaiswal <jaiswalswati94@gmail.com>
2014-08-16 18:00:25 +0530

    A script to download images from webpage

---
+++ image__downloader/README.md
@@ -0,0 +1,29 @@
+get_image.py is a python script created for downloading pictures from web pages.
+* * *
+
+Problem statement :
+----------------- 
+To create a script which can download images from web pages provided one URL.
+Should go through all the links mentioned on that URL and also download images from there.
+
+Instructions:
+------------
+The script is convenient to run from command prompt.
+It requires standard input.
+First input is the URL, of the main page.
+Second input is the directory name, where to save the images.
+
+Standard modules used:
+---------------------
+OS module is used.
+It provides the script with some facilities of os, such as executing commands.
+In the script, it is used for listing files in directories, and checking existence of directories.
+For more information on OS, read the [documentation of OS][] .
+
+urllib provides a high level interface for fetching data across the world wide web.
+For more information on OS, read the [documentation of urllib][]
+
+Rest and required information is provided in the script itself.
+
+[documentation of OS]: https://docs.python.org/2/library/os.html
+[documentation of urllib]:https://docs.python.org/2/library/urllib.html
---
+++ image__downloader/get_images.py
@@ -0,0 +1,72 @@
+"""
+Script is used to download images from webpages.
+"""
+import os, urllib
+
+url = raw_input("Enter the url: ")
+dir_ = raw_input("Enter the directory, where you want to save: ")
+
+if not os.path.isdir(dir_):
+    os.mkdir(dir_)
+    
+def get_image_url(page):
+    """
+    Returns image urls found on web page. 
+    """
+    start = page.find("src=")
+    if start == -1:
+        return None,0
+    url_start = page.find('"',start+1)
+    url_end = page.find('"',url_start+1)
+    url = page[url_start:url_end]
+    return url,url_end
+
+def extract_name(url):
+    """
+    Extracts the name of the image form provided url.
+    """
+    name = ""
+    for ch in url[-1::-1]:
+        if ch == "/":
+            break
+        name+=ch
+        
+    return name[-1::-1]
+
+def get_page(page):
+    '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    ''This module calls urlopen to collect the content of the input
+    page from web.
+    then returns that content to calling function.
+    If any network error occurs and page is not fetched, it provides
+    the user with suitable message.''
+    '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    print "entered get_page"
+    try:
+        return urllib.urlopen(page).read()
+    except:
+        print "There was some problem fetching the file."
+        return None
+
+def main(home_url,dir_):
+
+    crawled = []
+    page = get_page(home_url)                                               #converting the page into pdf.
+    if page:
+        while True:
+            url,end = get_image_url(page)
+            print url
+            if url:
+                if url.endswith(".jpg") or url.endswith(".jpeg"):
+                    if not url.startswith("http"):
+                        url = home_url+url[2:]
+                    name = extract_name(url)
+                    print name
+                    urllib.urlretrieve(url,dir_+"/"+name)
+                page = page[end:]
+            else:
+                break
+            #links = get_urls(page)                                        #getting all the links form the page.
+            
+if __name__ == "__main__":
+    main(url,dir_)
