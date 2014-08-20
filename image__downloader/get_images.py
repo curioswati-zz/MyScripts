86290ce64a8cb370f74c82a8a60c72dea3efa522
swati12jaiswal <jaiswalswati94@gmail.com>
2014-08-20 16:24:43 +0530

    now recursively go through each url found on page and collect all the links and downloads all images found there also

--- image__downloader/get_images.py
+++ image__downloader/get_images.py
@@ -1,13 +1,12 @@
 """
 Script is used to download images from webpages.
 """
-import os, urllib
+import os, urllib, get_urls
 
 url = raw_input("Enter the url: ")
 dir_ = raw_input("Enter the directory, where you want to save: ")
 
-if not os.path.isdir(dir_):
-	print dir_
+if not os.path.isdir(dir_):                                             #creating directory specified by input
 	os.mkdir(dir_)
 
 def get_image_url(page):
@@ -22,6 +21,23 @@ def get_image_url(page):
     url = page[url_start:url_end]
     return url,url_end
 
+def get_images(home_url,page,path):
+	"""
+    Downloads images using image_urls found on page. 
+    """
+	while True:
+            url,end = get_image_url(page)
+            if url:
+                if url.endswith(".jpg") or url.endswith(".jpeg"):
+                    if not url.startswith("http"):
+                        url = home_url+url[2:]
+                    name = extract_name(url)
+                    print name
+                    urllib.urlretrieve(url,path+"/"+name)
+                page = page[end:]
+            else:
+                break
+	
 def extract_name(url):
     """
     Extracts the name of the image form provided url.
@@ -34,6 +50,17 @@ def extract_name(url):
         
     return name[-1::-1]
 
+def union(a,b):
+    '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    ''This module combines two input lists.
+    Checks for each entry in one list, if that does not exist in second
+    list; it appends it to the second list.''
+    '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    print "entered union"
+    for e in b:
+        if e not in a:
+            a.append(e)
+            
 def get_page(page):
     '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
     ''This module calls urlopen to collect the content of the input
@@ -52,22 +79,25 @@ def get_page(page):
 def main(home_url,dir_):
 
     crawled = []
-    page = get_page(home_url)                                               #converting the page into pdf.
+    page = get_page(home_url)                                         #fetching content of page, so that, we can collect urls from page.
     if page:
-        while True:
-            url,end = get_image_url(page)
-            print url
-            if url:
-                if url.endswith(".jpg") or url.endswith(".jpeg"):
-                    if not url.startswith("http"):
-                        url = home_url+url[2:]
-                    name = extract_name(url)
-                    print name
-                    urllib.urlretrieve(url,dir_+"/"+name)
-                page = page[end:]
-            else:
-                break
-            #links = get_urls(page)                                        #getting all the links form the page.
+		get_images(home_url,page,dir_)                                                  #downloading images from home_url
+    if page:
+		links = get_urls.main(page)                                        #getting all the links form the page.
+		if links:
+			for url in links:                                       #iterating over the links in the list of links.
+				if url not in crawled:
+					crawled.append(url)
+					if not url.startswith("http"):
+					    url = home_url+url                          #the url of the locally referenced page.
+					
+					page = get_page(url)
+					get_images(home_url,page,dir_)                    		  #converting the page
+					if page:                                          #if page was fetched.  
+						union(links,get_urls.main(page))              #union all new links from the page to old list of urls.
+		else:
+			print "There were no links on "+url
+    
             
 if __name__ == "__main__":
     main(url,dir_)
